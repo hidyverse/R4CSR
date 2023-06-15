a88ee4ed3291b548aa1e98fadb1a5ecab620b45f
Heidi <heidiesteiner@gmail.com>
2023-06-15 13:04:27 -0700

    Update specificAE.R

--- R/specificAE.R
+++ R/specificAE.R
@@ -28,12 +28,3 @@ t1 <- ana %>%
   summarise(n = fmt_num(n_distinct(USUBJID), digits = 0)) %>%
   mutate(AEDECOD = AESOC, order = 0)
 
-t1 %>% head(4)
-#> # A tibble: 4 × 5
-#> # Groups:   TRTAN [1]
-#>   TRTAN AESOC                       n      AEDECOD                     order
-#>   <dbl> <chr>                       <chr>  <chr>                       <dbl>
-#> 1     0 Cardiac Disorders           "  13" Cardiac Disorders               0
-#> 2     0 Ear and Labyrinth Disorders "   1" Ear and Labyrinth Disorders     0
-#> 3     0 Eye Disorders               "   4" Eye Disorders                   0
-#> 4     0 Gastrointestinal Disorders  "  17" Gastrointestinal Disorders      0
